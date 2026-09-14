4f1d447621a8197ef031928c65fdebdb1bf60de5
Regina <regina_pm@MacBook-Air-2.local>
2026-09-14 22:57:48 +0300

    Lab 1 - stack buffer overflow exploit

---
+++ target.c
@@ -0,0 +1,21 @@
+// lab1 target.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void xjlokjlx() { puts("Kitty says xjlokjlx!"); }
+void cimbtsgm() { puts("Kitty says cimbtsgm!"); }
+void win() { execv("/bin/sh", 0); }
+void oodbuxxt() { puts("Kitty says oodbuxxt!"); }
+void imauzenj() { puts("Kitty says imauzenj!"); }
+
+int main() {
+    int pwd[16] = { 0 };
+    char buf[16] = { 0 };
+
+    gets(buf);
+    if(pwd[0] != 1337)
+        exit(1);
+    else
+        puts("ACCESS GRANTED!");
+}
